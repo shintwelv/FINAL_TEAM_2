35132db6f38ededf311e88ddf11fc4c1908c306d
shintwl <shinilheo0205@gmail.com>
2021-11-23 13:02:35 +0900

    자유게시판 댓글 select 확인

--- BangBangGokGok_springBoot/src/main/java/com/kosmo/project/free/reply/controller/FreeReplyController.java
+++ BangBangGokGok_springBoot/src/main/java/com/kosmo/project/free/reply/controller/FreeReplyController.java
@@ -3,107 +3,62 @@ package com.kosmo.project.free.reply.controller;
 import java.util.Date;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.kosmo.project.free.reply.model.FreeReplyVO;
 import com.kosmo.project.free.reply.service.FreeReplyService;
 
-@Controller
-@RequestMapping(value = "/free")
+@RestController
+@RequestMapping(value = "/free/reply")
+@CrossOrigin
 public class FreeReplyController {
 
 	@Autowired
 	private FreeReplyService service;
 
-	@RequestMapping(value = {"/", "/main"})
-	public ModelAndView goMain() {
-		List<FreeReplyVO> replies = service.getAllReplies();
-		ModelAndView mv = new ModelAndView("main");
-		mv.addObject("replies", replies);
-		return mv;
-	}
-
-	@RequestMapping(value = "/insert.do")
-	public String fwdInsert() {
-		return "insert";
-	}
-
-	@RequestMapping(value = "/update.do")
-	public ModelAndView fwdUpdate(HttpServletRequest request) {
-		ModelAndView mv = getMV(request);
-
-		mv.setViewName("update");
-		return mv;
-	}
-
-	@RequestMapping(value = "/delete.do")
-	public ModelAndView fwdDelete(HttpServletRequest request) {
-		ModelAndView mv = getMV(request);
-
-		mv.setViewName("delete");
-		return mv;
-	}
-
 	@RequestMapping(value = "/view.do")
-	public ModelAndView fwdView(HttpServletRequest request) {
-		int articleId = Integer.parseInt(request.getParameter("articleId"));
-		int totalPage = getReplyPageNum(articleId);
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("view");
-		List<FreeReplyVO> replies = service.getReplies(articleId);
-
-		mv.addObject("replies", replies);
-		mv.addObject("totalPage", totalPage);
-		return mv;
-	}
-
-	private ModelAndView getMV(HttpServletRequest request) {
-		ModelAndView mv = new ModelAndView();
-		int articleId = Integer.parseInt(request.getParameter("articleId"));
-		int replyId = Integer.parseInt(request.getParameter("replyId"));
-
-		FreeReplyVO reply = service.getReply(replyId, articleId);
-
-		mv.addObject("reply", reply);
-
-		return mv;
+	public List<FreeReplyVO> getAllRepliesOfBoard(FreeReplyVO vo) {
+		List<FreeReplyVO> replies = service.getReplies(vo.getArticleId());
+		return replies;
+		
 	}
 	
 	@RequestMapping(value = "/insertProcess.do")
-	public ModelAndView insertProcess(FreeReplyVO vo) {
-		
-		vo.setReplyDate(new Date());
-		
-		service.makeReply(vo);
-		
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("redirect:main");
-		
-		return mv;
+	public boolean insertProcess(FreeReplyVO vo) {
+		try {
+			vo.setReplyDate(new Date());
+			service.makeReply(vo);
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			return false;
+		}
 	}
 	
 	@RequestMapping(value = "/updateProcess.do")
-	public ModelAndView updateProcess(FreeReplyVO vo) {
-		vo.setReplyDate(new Date());
-		service.updateReply(vo);
-		
-		ModelAndView mv = new ModelAndView("redirect:main");
-		
-		return mv;
+	public boolean updateProcess(FreeReplyVO vo) {
+		try {
+			vo.setReplyDate(new Date());
+			service.updateReply(vo);
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			return false;
+		}
 	}
 	
 	@RequestMapping(value = "/deleteProcess.do")
-	public ModelAndView deleteProcess(FreeReplyVO vo) {
-		service.deleteReply(vo);
-		
-		ModelAndView mv = new ModelAndView("redirect:main");
-		
-		return mv;
+	public boolean deleteProcess(FreeReplyVO vo) {
+		try {
+			service.deleteReply(vo);
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			return false;
+		}
 	}
 	
 //	Pagination
