e66294e46a985c10df94488bdddfc9df7bb8bbfb
shintwl <shinilheo0205@gmail.com>
2021-11-25 18:07:36 +0900

    변수명 변경

--- BangBangGokGok_springBoot/src/main/java/com/kosmo/project/festival/board/controller/FestivalController.java
+++ BangBangGokGok_springBoot/src/main/java/com/kosmo/project/festival/board/controller/FestivalController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.kosmo.project.festival.board.model.FestivalVO;
 import com.kosmo.project.festival.board.service.FestivalService;
-import com.kosmo.project.festival.util.Constants;
+import com.kosmo.project.util.Constants;
 
 @RestController
 @RequestMapping(value = "/festival")
--- BangBangGokGok_springBoot/src/main/java/com/kosmo/project/festival/board/service/FestivalServiceimpl.java
+++ BangBangGokGok_springBoot/src/main/java/com/kosmo/project/festival/board/service/FestivalServiceimpl.java
@@ -9,28 +9,27 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.kosmo.project.festival.board.model.FestivalVO;
 import com.kosmo.project.festival.board.repository.FestivalRepository;
-import com.kosmo.project.festival.util.Constants;
+import com.kosmo.project.util.Constants;
 
 @Service
 public class FestivalServiceimpl implements FestivalService{
 	@Autowired 
-	private FestivalRepository festi;
+	private FestivalRepository repo;
+	
+	private String projectPath = Constants.FESTIVAL_IMAGE_LOCATION;
 	
 	@Override
 	public List<FestivalVO> getAllFestival(){
-		return festi.findAll();
+		return repo.findAll();
 	}
 	
 	@Override
 	public FestivalVO getFindFestival(int festivalId){
-		return festi.findByFestivalId(festivalId);
+		return repo.findByFestivalId(festivalId);
 	}
 	
 	@Override
 	public void updateFestival(FestivalVO vo, MultipartFile file) throws Exception{ 
-		String projectPath = Constants.FESTIVAL_IMAGE_LOCATION;
-		
-		
 		String fileName = file.getOriginalFilename();
 		
 		File saveFile = new File(projectPath, fileName);
@@ -39,19 +38,17 @@ public class FestivalServiceimpl implements FestivalService{
 		
 		vo.setFestivalImage("/festival_image_location/"+fileName);
 		
-		festi.save(vo);
+		repo.save(vo);
 	}
 	
 	@Override
 	public void deleteFestival(FestivalVO vo) {
-		festi.delete(vo);
+		repo.delete(vo);
 	}
+	
 	//글 작성 처리
 	@Override
 	public void insertFestival(FestivalVO vo, MultipartFile file) throws Exception{
-		String projectPath = Constants.FESTIVAL_IMAGE_LOCATION;
-		
-		
 		String fileName = file.getOriginalFilename();
 		
 		File saveFile = new File(projectPath, fileName);
@@ -60,13 +57,13 @@ public class FestivalServiceimpl implements FestivalService{
 		
 		vo.setFestivalImage("/festival_image_location/"+fileName);
 		
-		festi.save(vo);
+		repo.save(vo);
 	}
 	
 	@Override
 	public List<FestivalVO> boardSearchList(String searchKeyword) {
 		
-		return festi.findByfestivalNameContaining(searchKeyword);
+		return repo.findByfestivalNameContaining(searchKeyword);
 		
 	}
 	
