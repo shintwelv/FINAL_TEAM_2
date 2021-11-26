e4160547d1f8d9f54a2986fc8ec59049559a9331
shintwl <shinilheo0205@gmail.com>
2021-11-26 12:39:56 +0900

    조회수

--- BangBangGokGok_springBoot/src/main/java/com/kosmo/project/festival/board/service/FestivalServiceimpl.java
+++ BangBangGokGok_springBoot/src/main/java/com/kosmo/project/festival/board/service/FestivalServiceimpl.java
@@ -15,58 +15,60 @@ import com.kosmo.project.util.Constants;
 public class FestivalServiceimpl implements FestivalService{
 	@Autowired 
 	private FestivalRepository repo;
-	
+
 	private String ImgLocation = Constants.DEFAULT_DIR + "festival";
 	private String ImgLocation_for_DB = "./fesitval/";
-	
+
 	@Override
 	public List<FestivalVO> getAllFestival(){
 		return repo.findAll();
 	}
-	
+
 	@Override
 	public FestivalVO getFindFestival(int festivalId){
 		return repo.findByFestivalId(festivalId);
 	}
-	
+
 	@Override
 	public void updateFestival(FestivalVO vo, MultipartFile file) throws Exception{ 
-		String fileName = file.getOriginalFilename();
-		
-		File saveFile = new File(ImgLocation, fileName);
-		
-		file.transferTo(saveFile);
-		
-		vo.setFestivalImage(ImgLocation_for_DB + fileName);
-		
+		if (file != null) {
+			String fileName = file.getOriginalFilename();
+
+			File saveFile = new File(ImgLocation, fileName);
+
+			file.transferTo(saveFile);
+
+			vo.setFestivalImage(ImgLocation_for_DB + fileName);
+		}
+
 		repo.save(vo);
 	}
-	
+
 	@Override
 	public void deleteFestival(FestivalVO vo) {
 		repo.delete(vo);
 	}
-	
+
 	//글 작성 처리
 	@Override
 	public void insertFestival(FestivalVO vo, MultipartFile file) throws Exception{
 		String fileName = file.getOriginalFilename();
-		
+
 		File saveFile = new File(ImgLocation, fileName);
-		
+
 		file.transferTo(saveFile);
-		
+
 		vo.setFestivalImage(ImgLocation_for_DB + fileName);
-		
+
 		repo.save(vo);
 	}
-	
+
 	@Override
 	public List<FestivalVO> boardSearchList(String searchKeyword) {
-		
+
 		return repo.findByfestivalNameContaining(searchKeyword);
-		
+
 	}
-	
-	
+
+
 }
